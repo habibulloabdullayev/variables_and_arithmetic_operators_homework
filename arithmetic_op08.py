540a25454760d12b1bddc57a316fe51afe3b57e9
habibulloabdullayev <habibulloabdullayev02@gmail.com>
2023-07-12 10:28:16 +0500

    arithmetic_op08

--- arithmetic_op08.py
+++ arithmetic_op08.py
@@ -1,7 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 3.
-
+a = int(3)
 # Create a variable "b"  and assign it the value of the int 4.
-
+b = int(4)
 # Create a variable "c"  and assign it the value of the int 2.
-
+c = int(2)
 # Create a variable called 'answer' and assign it the expression value: a*b:c
+answer = a*b/c
