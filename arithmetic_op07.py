052c56e21f855050be90564634a7d44d4232931e
habibulloabdullayev <habibulloabdullayev02@gmail.com>
2023-07-12 10:24:51 +0500

    arithmetic_op07

--- arithmetic_op07.py
+++ arithmetic_op07.py
@@ -1,7 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 12.
-
+a = int(12)
 # Create a variable "b"  and assign it the value of the int 4.
-
+b = int(4)
 # Create a variable "c"  and assign it the value of the int 1.
-
-# Create a variable called 'answer' and assign it the expression value: a - b + c
+c = int(1)
+# lled 'answer' and assign it the expression value: a - b + cCreate a variable ca
+answer = a - b + c
